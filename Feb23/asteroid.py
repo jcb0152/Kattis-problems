eb51c4f84ce18bbac6e9824a0cc9f8aa08a4fbcf
jcb0152 <jcb0152@auburn.edu>
2023-02-08 18:37:07 -0600

    Adding comments

--- Feb23/asteroid.py
+++ Feb23/asteroid.py
@@ -1,5 +1,10 @@
+# Task: Determine if cylinder intersects with line segment
+# Strategy: Check if distance between line segment and axis of cylinder is less than the cylinder radius.
+# Compare at endpoint of line segment, cylinder, and shortest line (if extending the ray and line segment)
 from collections import deque
 
+# Defining functions for vector arithmetic
+# cross product
 def cross(a, b):
     (ax, ay, az) = a
     (bx, by, bz) = b
@@ -8,26 +13,32 @@ def cross(a, b):
     z = (ax * by) - (ay * bx)
     return (x, y, z)
 
+# dot product
 def dot(a, b):
     (ax, ay, az) = a
     (bx, by, bz) = b
     return ((ax*bx) + (ay*by) + (az*bz))
 
+# distance between points
 def distance(a, b):
     (ax, ay, az) = a
     (bx, by, bz) = b
     return ((ax - bx) ** 2 + (ay - by) ** 2 + (az - bz) ** 2) ** 0.5
 
+# find point on vector from parametric equations
+# a is origin, b is direction, t is parameter
 def param(a, b, t):
     (ax, ay, az) = a
     (bx, by, bz) = b
     return ((ax + bx * t), (ay + by * t), (az + bz * t))
 
+# Compute direction vector from a to b
 def between(a, b):
     (ax, ay, az) = a
     (bx, by, bz) = b
     return ((bx - ax), (by - ay), (bz - az))
 
+# Compute projection of b onto vector a
 def project(a, a_dir, b):
     b_t = dot(between(a, b), a_dir) / dot(a_dir, a_dir)
     b_proj = param(a, a_dir, b_t)
@@ -69,11 +80,12 @@ for (a_point, a_dir, r) in asteroids:
 
     d_short = float("inf")
     
-    # if not parallel
+    # if not parallel, check the closest point between lines defined by vector
     if n != (0,0,0):
         s_par = dot(cross(a_dir, n), t) / dot(n, n)
         a_par = dot(cross(s_dir, n), t) / dot(n, n)
 
+        # Ensure that all points are on line segment or cylinder axis
         s_par = max(min(s_par, 1), 0)
         a_par = max(a_par, 0)
 
@@ -87,9 +99,12 @@ for (a_point, a_dir, r) in asteroids:
 
     d_start = distance(a_point, s_point)
 
+    # Calculate distance between ship and projection of ship onto asteroid vector
+    # and vice versa
     (ship_t, proj_ship_ast) = project(a_point, a_dir, s_point)
     (ast_t, proj_ast_ship) = project(s_point, s_dir, a_point)
 
+    # Ignore projection points behind starting point
     if ship_t >= 0:
         d_ship = distance(proj_ship_ast, s_point)
 
