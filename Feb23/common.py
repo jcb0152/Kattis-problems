e0ba3ed99a23bae13eb00a65cc4329fdf78d076d
jcb0152 <jcb0152@auburn.edu>
2023-02-09 16:45:33 -0600

    Adding comments

--- Feb23/common.py
+++ Feb23/common.py
@@ -1,3 +1,7 @@
+# Task: Find the number with the greatest fraction of common factors less than the input number
+# Strategy: Multiply prime numbers until almost past input number. Compute cototient of this number.
+# Definitions: Totient = number of integers less than n coprime with n (count k in [1..n] where gcd(k, n) == 1)
+# Cototient = number of integers less than n not coprime with n (count k in [1..n] where gcd(k, n) > 1)
 import math
 
 num = int(input())
@@ -7,6 +11,7 @@ counter = 1
 
 primes = []
 
+# Multiply prime numbers together, saving previous factors.
 while True:
     x = math.gcd(ans, counter)
         
@@ -19,12 +24,14 @@ while True:
         ans = tmp
         primes.append(counter)
     counter += 1
-    
+   
+# Compute totient using product(1-1/i) for each prime factor i
 frac = ans
 for i in primes:
     frac *= ((i - 1) / i)
     frac = round(frac)
 
+# cototient = num - totient
 frac = ans - frac
 common = math.gcd(frac, ans)
 numer = round(frac / common)
