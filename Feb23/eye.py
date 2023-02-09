d40ad71e9049fd3107ad3b4c682be4909332655a
jcb0152 <jcb0152@auburn.edu>
2023-02-09 16:48:21 -0600

    Adding comments

--- Feb23/eye.py
+++ Feb23/eye.py
@@ -1,6 +1,9 @@
+# Task: Determine if there is an equal number of "|" characters on each side of the "()" characters
+# Strategy: Count number of "|" characters before "(" character. Then, compare to amount of "|" characters in the string.
 x = input()
 
 lhs = x.index("(")
+# See if lhs equals half of the "|" characters in the string
 if (len(x) - 2) / 2 != lhs:
     print("fix")
 else:
