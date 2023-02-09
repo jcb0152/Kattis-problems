2ac1189529bf16c20e6f4ba13d6f03bd8736699c
jcb0152 <jcb0152@auburn.edu>
2023-02-09 17:00:36 -0600

    Adding comments

--- Feb23/mult.py
+++ Feb23/mult.py
@@ -1,5 +1,8 @@
+# Task: Determine first number in a sequence that is a multiple of the starting number. Start a new sequence after a multiple is found.
+# Strategy: Calculate (modulo n) for each integer in the sequence. Use new n after multiple is found.
 num = int(input())
 
+# Store modulo base in first. Set change flag if the next number will change the modulo base.
 first = 0
 change = True
 for _ in range(num):
@@ -10,6 +13,7 @@ for _ in range(num):
         first = tmp
         continue
 
+    # When a multiple is found, print it and indicate that a new sequence is starting.
     if tmp % first == 0:
         print(tmp)
         change = True
