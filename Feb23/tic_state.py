b32de4f27eebdb050fb288b2922c03c80b0b985a
jcb0152 <jcb0152@auburn.edu>
2023-02-09 17:41:14 -0600

    Adding comments

--- Feb23/tic_state.py
+++ Feb23/tic_state.py
@@ -1,6 +1,9 @@
+# Task: Given a tic-tac-toe board as an octal number, determine the current state of the game (x win, o win, cat's game, in progress)
+# Strategy: Parse input, then compare indicies of values in potential winning positions
 cases = int(input())
 
 for _ in range(cases):
+    # Parsing input
     num = str(bin(int(input(), 8)))[2:]
 
     num = "0"*(19-len(num)) + num
@@ -11,12 +14,15 @@ for _ in range(cases):
     x = num[9:18]
     turn = num[18]
 
+    # Array of potential winning positions
     win = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
 
     for tup in win:
+        # Check if all values in each winning position are occupied.
         if filled[tup[0]] == "0" or filled[tup[1]] == "0" or filled[tup[2]] == "0":
             continue
 
+        # If values in each winning position are equal, determine winner.
         if x[tup[0]] == x[tup[1]] and x[tup[0]] == x[tup[2]]:
             winner = "X"
             if x[tup[0]] == "0":
@@ -25,7 +31,9 @@ for _ in range(cases):
             print(f"{winner} wins")
             break
     else:
+        # If all spaces are filled with no winner, it is a cat's game.
         if sum([int(i) for i in filled]) == 9:
             print("Cat\'s")
+        # Otherwise, it is in progress.
         else:
             print("In progress")
